560143f36c5c32f696e54b6edde6efadbb4c5f47
nezaicev <nezaicev@yandex.ru>
2023-04-22 03:45:30 +0300

    add Admin model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from users.models import CustomUser
 # Register your models here.
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model=CustomUser
+    list_display = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
